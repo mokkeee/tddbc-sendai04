b84d18aef477d5b6ba6ee8754e252da2b1cd54e5
ゲスト（AppRuns） <guest.appruns@gmail.com>
2014-10-11 13:31:54 +0900

    リファクタリング

--- src/test/java/tddbc/PersonTest.java
+++ src/test/java/tddbc/PersonTest.java
@@ -1,61 +1,72 @@
 package tddbc;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-/**
- * Created by guest on 2014/10/11.
- */
+@RunWith(Enclosed.class)
 public class PersonTest {
 
-	@Test
-	public void 佐藤太郎を生成できる() throws Exception {
-		Person sut = new Person("佐藤", "太郎");
-		assertThat(sut, is(instanceOf(Person.class)));
-	}
+    public static class 佐藤太郎の場合 {
+        Person sut;
 
-    @Test
-    public void 佐藤太郎の名字は佐藤である() throws Exception {
-		Person sut = new Person("佐藤", "太郎");
-        assertThat(sut.getFamilyName(), is("佐藤"));
+        @Before
+        public void setUp() throws Exception {
+            sut = new Person("佐藤", "太郎");
+        }
+
+        @Test
+        public void 佐藤太郎を生成できる() throws Exception {
+            assertThat(sut, is(instanceOf(Person.class)));
+        }
+
+        @Test
+        public void 佐藤太郎の名字は佐藤である() throws Exception {
+            assertThat(sut.getFamilyName(), is("佐藤"));
+        }
+
+        @Test
+        public void 佐藤太郎の名前は太郎である() throws Exception {
+            assertThat(sut.getFirstName(), is("太郎"));
+        }
+
+        @Test
+        public void 佐藤太郎の氏名は佐藤太郎である() throws Exception {
+            assertThat(sut.getFullName(), is("佐藤太郎"));
+        }
     }
 
-	@Test
-	public void 佐藤太郎の名前は太郎である() throws Exception {
-		Person sut = new Person("佐藤", "太郎");
-		assertThat(sut.getFirstName(), is("太郎"));
-	}
+    public static class 名字または名前がない人は生成できない {
 
-    @Test
-    public void 佐藤太郎の氏名は佐藤太郎である() throws Exception {
-		Person sut = new Person("佐藤", "太郎");
-        assertThat(sut.getFullName(), is("佐藤太郎"));
+        @Test(expected = IllegalArgumentException.class)
+        public void 名字がない人は生成できない() throws Exception {
+            Person sut = new Person("", "太郎");
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void 名前がない人は生成できない() throws Exception {
+            Person sut = new Person("佐藤", "");
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void 名字と名前がない人は生成できない() throws Exception {
+            Person sut = new Person("", "");
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void 名字がnullの人は生成できない() throws Exception {
+            new Person(null, "太郎");
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void 名前がnullの人は生成できない() throws Exception {
+            Person sut = new Person("佐藤", null);
+        }
+
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void 名字がない人は生成できない() throws Exception {
-        Person sut = new Person("", "太郎");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void 名前がない人は生成できない() throws Exception {
-        Person sut = new Person("佐藤", "");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void 名字と名前がない人は生成できない() throws Exception {
-        Person sut = new Person("", "");
-    }
-
-	@Test(expected = IllegalArgumentException.class)
-	public void 名字がnullの人は生成できない() throws Exception {
-		new Person(null, "太郎");
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void 名前がnullの人は生成できない() throws Exception {
-		Person sut = new Person("佐藤", null);
-	}
 }
